4493c4c0a2a6fc6c0071c14517ddb546035b5e46
Jasparpirs <jaspar@hkhk.edu.ee>
2024-11-07 21:10:16 +0200

    Create 9.14.py

---
+++ 9.14.py
@@ -0,0 +1,167 @@
+import turtle
+turtle.speed(10)
+#6
+
+"""
+for i in range (4):
+    turtle.lt(90)
+    for i in range(2):
+        turtle.fd(120)
+        turtle.left(90)
+        turtle.fd(120/2)
+        turtle.left(90)
+
+#12
+turtle.penup()
+turtle.goto(400,0)
+turtle.pendown()
+
+for i in range(3):
+    turtle.fd(50)
+    turtle.lt(120)
+
+turtle.penup()
+turtle.goto(450,0)
+turtle.pendown()
+
+for i in range (3):
+    turtle.fd(50)
+    turtle.rt(120)
+
+turtle.rt(60)
+turtle.fd(100)
+turtle.rt(120)
+turtle.fd(50)
+turtle.rt(120)
+turtle.fd(50)
+turtle.rt(30)
+
+turtle.penup()
+turtle.goto(400,0)
+turtle.pendown()
+
+turtle.lt(150)
+for i in range(3):
+    turtle.fd(50)
+    turtle.lt(120)
+
+turtle.lt(120)
+for i in range(3):
+    turtle.fd(50)
+    turtle.rt(120)
+
+for i in range(3):
+    turtle.fd(50)
+    turtle.lt(120)
+
+turtle.fd(50)
+turtle.lt(60)
+for i in range(3):
+    turtle.fd(50)
+    turtle.lt(120)
+
+for i in range(3):
+    turtle.rt(120)
+    turtle.fd(50)
+
+turtle.rt(60)
+for i in range(3):
+    turtle.fd(50)
+    turtle.rt(120)
+  
+turtle.rt(60)
+turtle.fd(50)
+turtle.rt(60)
+for i in range(3):
+    turtle.fd(50)
+    turtle.rt(120)
+
+for i in range(3):
+    turtle.lt(120)
+    turtle.fd(50)
+"""
+
+
+  
+
+
+
+
+#7
+turtle.penup()
+turtle.goto(300,0)
+turtle.pendown()
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+
+turtle.fd(25)
+turtle.rt(45)
+for i in range(2):
+    turtle.fd(25)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+
+
+
+
+
+
+
+    
+    
