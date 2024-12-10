2d73f0881cb8b960d969a51469e14fa3ea51703b
Jasparpirs <jaspar@hkhk.edu.ee>
2024-12-10 11:42:38 +0200

    wdadawdaw

---
+++ kordamine/4.1.py
@@ -0,0 +1,6 @@
+mitu = int(input("Mitu korda: "))
+lause = input("Sinu reklaamlause : ")
+
+def banner[l]
+for i in range (mitu):
+    banner(lause)
---
+++ kordamine/4.2.py
@@ -0,0 +1,5 @@
+def mahlapakkide_arv(ountekogus):
+    mahlapakkidearv = int(round(ountekogus * 0.4/3,0))
+    return mahlapakkide_arv
+kg = int(input("ounte kogus"))
+print(mahlapakkide_arv(kg))
