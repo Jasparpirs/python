00f17c79cb019336790365a78158bffbbf5622e9
Jasparpirs <jaspar@hkhk.edu.ee>
2024-12-12 09:57:30 +0200

    d<wd<

---
+++ kordamine/4.3.py
@@ -0,0 +1,10 @@
+def eelarve(kylalised):
+    kylalised = int(input("mitu inimest on kutsutud: ? "))
+    eelarve = kylalised * 10 + 55
+    return eelarve
+
+# max_inimesed =int(input("mitu on kutsutud?"))
+# min_inimesed = int(input("mitu inimest tuleb?"))
+
+print(eelarve(max_inimesed =int(input("mitu on kutsutud?"))))
+print(eelarve(min_inimesed = int(input("mitu inimest tuleb?"))))
---
+++ kordamine/4.4.py
@@ -0,0 +1,7 @@
+def tervitus(n):
+    for i in range(n):
+        print(f"Täna {n}kord tervitada, mõtisköeb võõrustaja")
+        print("Külaline:Tere, suur tänu kutse eest! ")
+        print("Võõrustaja: Tere!")
+        
+tervitus(10)
