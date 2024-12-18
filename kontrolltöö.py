6eb591c1ee8aa8358127f73f396c8a122baf982a
Jasparpirs <jaspar@hkhk.edu.ee>
2024-12-18 14:00:26 +0200

    Create kontrolltöö.py

---
+++ kontrolltöö.py
@@ -0,0 +1,63 @@
+import random
+#Jaspar 
+#18.12.2024
+#1 4 8 5 3
+
+#1
+# oige = 0
+# for i in range(10):
+#     üks = random.randint(1, 10)
+#     kaks = random.randint(1, 10)
+#     vastus = üks * kaks
+
+#     sisestatud_vastus = int(input(f"palju on {üks}*{kaks}? :"))
+
+# if vastus == oige:
+#         print("Õige")
+# else:
+#       print("vale")
+
+# print(f"Sa said {oige} 10nest õigesti")
+
+
+
+#4
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# for i in a:
+#     if i < 5:
+#         print(i)
+
+# x = [i for i in a if i <5] 
+# print(x) 
+
+
+# #8
+
+# def täringu_veeretamine():
+#     return random.randint(1, 6)
+
+
+
+#5
+
+tooted = []
+while asjad := input('Mida poest vaja on ? '):
+    tooted.append(asjad)
+print(tooted)
+
+#3
+positiivsed = []
+negatiivsed = []
+
+
+for i in range(5):
+    number = int(input("Sisesta viis numbrit: "))
+    if number > 0:
+        positiivsed.append(number)
+    else: number < 0
+    negatiivsed.append(number)
+
+print("Positiivsed numbrid:", positiivsed)
+print("Positiivsed numbrid", positiivsed)
