63be4adbcd3dfb08dae22156bc203f91e7ff2c72
Jasparpirs <jaspar@hkhk.edu.ee>
2025-01-14 10:51:05 +0200

    Update 19.py

--- 19.py
+++ 19.py
@@ -1,12 +1,31 @@
 #jaspar
 #07.01.25
 
+
 import json
+
+kl_12=0
+kl_11=0
+kl_10=0
 
 with open('haridustulemused.json', 'r', encoding='utf-8') as file:
     opilased = json.load(file)
 
-print(opilased[nimi])
-# json_data = '{"name": "John Doe", "klass": 35, "city": "New York"}'
-# klass = extract_key_value(json_data, "klass")
-# print(klass) 
+for opilane in opilased:
+    if opilane['klass'] == '12':
+        kl_12+=1
+        print(opilane['nimi'])
+        for tegevus in opilane['tegevused']:
+            print(tegevus)
+        if opilane['klass'] == '11':
+            kl_11+=1
+        if opilane['klass'] == '10':
+            kl_10+=1
+
+print(f"12. klassis õpib {kl_12}")
+print(f"11. klassis õpib {kl_11}")
+print(f"10. klassis õpib {kl_10}")
+
+
+
+
